4edf89d629f2665363911d2008d2e1fd868de7e6
unknown <juanisimioli@gmail.com>
2021-04-17 22:10:18 -0300

    form parser layer

---
+++ multipart-form-data-parser.js
@@ -0,0 +1,10 @@
+const formidable = require("formidable");
+
+exports.formparser = (req) =>
+  new Promise(function (resolve, reject) {
+    const form = new formidable.IncomingForm({ keepExtensions: true });
+    form.parse(req, function (err, fields, files) {
+      if (err) return reject(err);
+      resolve({ fields, files });
+    });
+  });
